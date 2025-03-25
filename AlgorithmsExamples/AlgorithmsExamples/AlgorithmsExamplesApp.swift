5166aba8fd66584b499dfd8a8f3fb3af7843caae
Augusto Reis <areis@payfare.com>
2025-03-25 15:41:30 -0300

    feat: Init commit

---
+++ AlgorithmsExamples/AlgorithmsExamples/AlgorithmsExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsExamplesApp.swift
+//  AlgorithmsExamples
+//
+//  Created by Augusto Reis on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlgorithmsExamples/AlgorithmsExamplesTests/AlgorithmsExamplesTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsExamplesTests.swift
+//  AlgorithmsExamplesTests
+//
+//  Created by Augusto Reis on 25/03/25.
+//
+
+import Testing
+@testable import AlgorithmsExamples
+
+struct AlgorithmsExamplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
